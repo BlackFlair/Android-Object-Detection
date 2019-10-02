d3fb17e0d85319f1b5f313d609edb8efe408d5ad
BlackFlair <surinmachaiah@gmail.com>
2019-10-02 19:15:16 +0530

    ERROR : Package not found

--- openCVLibrary310/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary310/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\ZZZZZ\\Android Studio\\Code\\AndroidOpenCV\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: D:\\ZZZZZ\\Android Studio\\Code\\Android-Object-Detection\\openCVLibrary310\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
